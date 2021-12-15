66586881607b754f5265498c09c8135210772e5a
Nikita <n.udin@yandex.ru>
2021-12-15 08:50:53 +0400

    learn1

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-"use strict"; 
+
 
 /*const answers = [];
 
@@ -8,4 +8,26 @@ answers [2] = prompt('Сколько вам лет?', '');
 
 document.write(answers);*/
 
+'use strict'; 
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели','');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+      
+
+      console.log(personalMovieDB); 
